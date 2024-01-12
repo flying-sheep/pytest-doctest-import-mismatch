71e94b5370d6206748306999d875aa5259fb31be
Philipp A. <flying-sheep@web.de>
2024-01-12 17:02:02 +0100

    smaller

--- src/anndata/tests/test_base.py
+++ src/anndata/tests/test_base.py
@@ -1,15 +1,8 @@
 from __future__ import annotations
 
 
-from anndata import AnnData
-from anndata.tests.helpers import assert_equal, gen_adata
+from anndata.tests.helpers import gen_adata
 
 
 def test_attr_deletion():
-    full = gen_adata((30, 30))
-    # Empty has just X, obs_names, var_names
-    empty = AnnData(None, obs=full.obs[[]], var=full.var[[]])
-    for attr in ["X", "obs", "var", "obsm", "varm", "obsp", "varp", "layers", "uns"]:
-        delattr(full, attr)
-        assert_equal(getattr(full, attr), getattr(empty, attr))
-    assert_equal(full, empty, exact=True)
+    gen_adata((30, 30))
